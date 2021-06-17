70969b89d5a708d1ab09e6520ab8c6b0df488c1f
luisforgu <luis.forero@carvajal.com>
2021-06-17 14:07:33 -0500

    Arrays Clasificados

--- main.go
+++ main.go
@@ -1,64 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"sort"
+)
 
-func ordenarMayor(listNum []int, Cant int) {
-	tmp := 0
-	for x := 0; x < Cant; x++ {
-		for y := 0; y < Cant; y++ {
-			if listNum[x] > listNum[y] {
-				tmp = listNum[x]
-				listNum[x] = listNum[y]
-				listNum[y] = tmp
-			}
-		}
-	}
-	fmt.Print("\nArray dinamico ordenado: ")
-	for i := 0; i < Cant; i++ {
-		fmt.Print("[", listNum[i], "]")
-	}
-	fmt.Println()
+// Defino los tipos de Datos.
+type Numeros struct {
+	Valores []int
 }
-func ordenarMenor(listNum []int, Cant int) {
-	tmp := 0
-	for x := 0; x < Cant; x++ {
-		for y := 0; y < Cant; y++ {
-			if listNum[x] < listNum[y] {
-				tmp = listNum[y]
-				listNum[y] = listNum[x]
-				listNum[x] = tmp
-			}
-		}
-	}
-	fmt.Print("\nArray dinamico ordenado: ")
-	for i := 0; i < Cant; i++ {
-		fmt.Print("[", listNum[i], "]")
-	}
-	fmt.Println()
-}
+
+// Creamos una Instancia
 func main() {
-	listNum := make([]int, 0)
-	var cant, opt, valor int
-	fmt.Print("¿Cuantos valores desea ingresar?: ")
-	fmt.Scanf("%d", &cant)
-	for v := 1; v <= cant; v++ {
-		fmt.Print("Ingrese en valor #", v, ": ")
-		fmt.Scanf("%d", &valor)
-		listNum = append(listNum, valor)
-	}
-	fmt.Println("\nSelecciones una de las siguientes opciones")
-	fmt.Println("------------------------------------------")
-	fmt.Println("1) Ordenar de mayor a menor")
-	fmt.Println("2) Ordenar de menor a mayor")
-	fmt.Println("------------------------------------------")
-	fmt.Print("Opcion: ")
-	fmt.Scanf("%d", &opt)
-	switch opt {
-	case 1:
-		ordenarMayor(listNum, cant)
-	case 2:
-		ordenarMenor(listNum, cant)
-	default:
-		fmt.Println("Opcion no valida!!")
-	}
+	text := "{\"Valores\": [3, 5, 5, 6, 8, 3, 4, 4, 7, 7, 1, 1, 2]}"
+	bytes := []byte(text)
+	var resultados Numeros
+	json.Unmarshal(bytes, &resultados)
+	fmt.Printf("Sin Clasificar = %v", resultados.Valores)
+
+	/*El paquete `sort` de Go implementa ordenamiento para los tipos primitivos y definidos por el usuario.*/
+	ints := []int{3, 5, 5, 6, 8, 3, 4, 4, 7, 7, 1, 1, 2}
+	sort.Ints(ints)
+	crear_json, _ := json.Marshal(ints)
+	convertir_a_cadena := string(crear_json)
+	fmt.Println(" Clasificado = ", convertir_a_cadena)
+	// fmt.Println(" Clasificado = ", ints)
 }
